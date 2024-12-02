102ebc2153c99e6e65ea82f91e4725fd66cfdf2f
masudulalam <masuudulalam@gmail.com>
2024-12-02 10:55:51 +0600

    added basic click event handler in add money button

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,1 +1,9 @@
-console.log('addMoney.js is connected');
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoney = document.getElementById('input-add-money').value;
+
+    const pinNumber = document.getElementById('input-pin-number').value;
+    
+    console.log(addMoney, pinNumber);
+})
