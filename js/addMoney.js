83a781ff8817180bd8c275a2d3ad9ef4bc3ad898
masudulalam <masuudulalam@gmail.com>
2024-12-02 11:21:19 +0600

    add money event handler is complete now that event handler is add the money

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,8 +2,17 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
 
     const addMoney = document.getElementById('input-add-money').value;
-
+    const addMoneyNumber = parseFloat(addMoney);
     const pinNumber = document.getElementById('input-pin-number').value;
-    
-    console.log(addMoney, pinNumber);
+
+    // Wrong way to verify or valid pin number but this is time it is right.
+    if (pinNumber === '1234') {
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+
+        const newBalance = balanceNumber + addMoneyNumber;
+        document.getElementById('account-balance').innerText = newBalance;
+    } else {
+        alert('Invalid Pin!. Please try again.')
+    }
 })
